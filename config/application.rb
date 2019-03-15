ed156b7c86221b883fd4a49cb24fe3c82fc4ccb7
Soshi Koike <ls600hlsoushi@yahoo.co.jp>
2019-03-15 21:58:24 +0900

    やばい完全につまった

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,6 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-    	address: "smtp.gmail.com",
-    	port: 5897,
-    	user_name: 'Soshi Koike',
-    	password: 'uxndmejijtjanftc',
-    }
+
   end
 end
